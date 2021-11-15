35e8888c6e0b6cdf255d6c7d563543ac007536aa
Bhaneshwar Kumar <55010022+bhaneshwar@users.noreply.github.com>
2021-11-15 12:08:00 +0530

    updated for jenkins configuration

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,7 @@ public class Helloworld{
 public static void main(String arg[]){
 System.out.println(" hello world test on mac " );
 
-System.out.println(" hello world latest update... on 25oct" );
+System.out.println(" hello world latest update... on 15Nov new Item created on jenkins" );
 
 }
 }
